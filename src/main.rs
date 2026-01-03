ee4e5a388a8719413ce95bd0241dc35ef84a14f8
Goutham Bharadwaj <goutham.bharadwaj@proton.me>
2026-01-03 20:51:03 +0530

    read a command

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+const PROMPT: &'static str = "$ ";
+
 fn main() {
-    print!("$ ");
+
+    print!("{}", PROMPT);
     io::stdout().flush().unwrap();
+
+    let command = read_command();
+    execute(command);
+}
+
+fn read_command() -> String {
+    let mut cmd = String::new();
+    io::stdin().read_line(&mut cmd).unwrap();
+    cmd
+}
+
+fn execute(cmd: String) {
+    println!("{}: command not found", cmd.as_str().trim());
 }
