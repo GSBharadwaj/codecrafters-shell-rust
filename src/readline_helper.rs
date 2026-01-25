99a921d87d261b0a67fffc96884871e260a39aad
Goutham Bharadwaj <goutham.bharadwaj@proton.me>
2026-01-26 01:11:17 +0530

    Minor refactoring
    I did not like having a "state" in the helper. So, chose a different way
    to append the space.  Where, if it's only one match,
    Sending the result with space appended.
    Don't like this method either. But prefer it over mutation.

--- src/readline_helper.rs
+++ src/readline_helper.rs
@@ -1,14 +1,11 @@
-use rustyline::completion::{extract_word, Completer};
+use rustyline::completion::Completer;
 use rustyline::highlight::Highlighter;
 use rustyline::hint::Hinter;
-use rustyline::line_buffer::LineBuffer;
 use rustyline::validate::Validator;
-use rustyline::{Changeset, Context};
-use std::cell::Cell;
+use rustyline::Context;
 
 pub struct ReadLineHelper {
     commands: Vec<String>,
-    unique_match_for_completion: Cell<Option<bool>>,
 }
 
 impl ReadLineHelper {
@@ -21,7 +18,6 @@ impl Default for ReadLineHelper {
     fn default() -> Self {
         Self {
             commands : vec![],
-            unique_match_for_completion: Cell::new(None),
         }
     }
 }
@@ -35,28 +31,20 @@ impl Completer for ReadLineHelper {
             return Ok((0, Vec::new()))
         }
 
-        let (start, _) = extract_word(line, pos, None, |s|  s.is_whitespace());
-
         let mut matches: Vec<String> = self.commands
             .iter()
             .filter(|cmd| cmd.starts_with(input))
             .cloned().collect();
         matches.sort();
         matches.dedup();
-        self.unique_match_for_completion.set(Some(matches.len() == 1));
-        Ok((start, matches))
+
+        if matches.len() == 1 {
+            return Ok((0, vec![format!("{} ", &matches[0])]))
+        }
+
+        Ok((0, matches))
     }
 
-    fn update(&self, line: &mut LineBuffer, start: usize, elected: &str, cl: &mut Changeset) {
-        let completion =
-            match self.unique_match_for_completion.get() {
-                Some(true) => {
-                    format!("{} ", elected)
-                },
-                _ => elected.to_string()
-            };
-        line.replace(start..line.pos(), &completion, cl);
-    }
 }
 
 impl Hinter for ReadLineHelper { type Hint = String; }
